9955d4450adc232899bfd6f42f65ed39df0d0fed
Lefi Andri <lefiandri@Lefis-MacBook-Pro.local>
2022-07-07 14:43:39 +0700

    upload file

---
+++ application/controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class HomeController extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file HomeController.php */
+/* Location: ./application/controllers/HomeController.php */
